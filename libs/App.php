d9817d499deea7b3f7b0caf0da852c38b1e4e70d
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-08-30 01:10:52 +0200

    Created functions to insert, update and delete data from the database.

--- libs/App.php
+++ libs/App.php
@@ -2,17 +2,21 @@
 
 class App
 {
+    // Variables
     public string $host = DB_HOST;
     public string $user = DB_USER;
     public string $password = DB_PASS;
     public string $database = DB_NAME;
     public $port = DB_PORT;
     public $link;
-    // Connecting to database
+
+    // Functions:
+
     public function __construct()
     {
         $this->connect();
     }
+    // Connecting to database
     public function connect()
     {
         $this->link = new PDO("mysql:host=$this->host:$this->port;dbname=$this->database", $this->user, $this->password);
@@ -21,6 +25,7 @@ class App
             echo "Connected successfully";
         }
     }
+    // Selecting all the data from the database
     public function selectAll($query)
     {
         $statement = $this->link->query($query);
@@ -33,6 +38,7 @@ class App
             return null;
         }
     }
+    // Selecting a single row of data from the database
     public function selectOne($query)
     {
         $statement = $this->link->query($query);
@@ -43,6 +49,47 @@ class App
             return $result;
         } else {
             return null;
+        }
+    }
+    // Inserting data into a database
+    public function insert($query, $array, $path)
+    {
+        if ($this->validate($array) == "Empty") {
+            echo "<script>alert('One or more inputs are empty!')</script>";
+        } else {
+            $insert = $this->link->prepare($query);
+            $insert->execute($array);
+
+            header("Location:" . $path);
+        }
+    }
+    // Updating data in a database
+    public function update($query, $array, $path)
+    {
+        if ($this->validate($array) == "Empty") {
+            echo "<script>alert('One or more inputs are empty!')</script>";
+        } else {
+            $update = $this->link->prepare($query);
+            $update->execute($array);
+
+            header("Location:" . $path);
+        }
+    }
+    // Deleting data in a database
+    public function delete($query, $path)
+    {
+        $delete = $this->link->query($query);
+        $delete->execute($query);
+
+        header("Location:" . $path);
+    }
+    // Validating inputted data
+    public function validate($array)
+    {
+        if (in_array("", $array)) {
+            echo "Empty";
+        } else {
+            return true;
         }
     }
     public function disconnect()
